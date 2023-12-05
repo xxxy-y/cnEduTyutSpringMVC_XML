a40db1d8d12d502e703f05d92eec01e125b4fa32
羊羊 <wqt1922311386@outlook.com>
2023-12-05 13:36:16 +0800

    Initial commit

---
+++ src/main/java/com/example/cnedutyutspringmvc/HelloServlet.java
@@ -0,0 +1,35 @@
+package com.example.cnedutyutspringmvc;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+
+/**
+ * @author wqt
+ */
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    @Override
+    public void init() {
+        message = "Hello World!";
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
